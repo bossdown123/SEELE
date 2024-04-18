9f378fbd15c15b34be8b05bddff0a5af0b072616
Adam Holness <adamvibeo@gmail.com>
2024-04-18 17:10:50 +0000

    corrected import

--- order_logic.py
+++ order_logic.py
@@ -1,5 +1,4 @@
 from datetime import datetime, timedelta
-from IPython.display import display, clear_output
 from keras.models import load_model
 from time import sleep
 from alpaca.data.historical import StockHistoricalDataClient
